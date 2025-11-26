eb674db3a2e44dede20e43673ff6e90b5ccbc07c
Chéridanh <divintsiela@gmail.com>
2025-11-26 11:41:29 +0100

    chore: initial project setup

---
+++ src/main/java/com/cheridanh/googlewalletservice/GoogleWalletServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cheridanh.googlewalletservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleWalletServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoogleWalletServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cheridanh/googlewalletservice/GoogleWalletServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cheridanh.googlewalletservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogleWalletServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
